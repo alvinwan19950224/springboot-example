6a41407473ab59adb17c6ff806f0db64a74c1e37
alvinwan19950224 <44048983+alvinwan19950224@users.noreply.github.com>
2018-12-20 16:48:14 +0800

    Add files via upload
    init project

---
+++ src/test/java/org/alvin/wan/ExampleApplicationsTest.java
@@ -0,0 +1,15 @@
+package org.alvin.wan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleApplicationsTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
